d45d1dcd96fc2ddb833eed48a16f6bff1c61a094
babyboy <bugs.bunny.shah@gmail.com>
2020-09-12 11:52:49 -0500

    oauth_agent

--- src/main/java/io/bugsbunny/endpoint/OAuthAgent.java
+++ src/main/java/io/bugsbunny/endpoint/OAuthAgent.java
@@ -1,6 +1,9 @@
 package io.bugsbunny.endpoint;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import io.bugsbunny.data.history.service.PayloadReplayService;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +13,9 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Provider
 public class OAuthAgent implements ContainerRequestFilter, ContainerResponseFilter
@@ -20,6 +25,13 @@ public class OAuthAgent implements ContainerRequestFilter, ContainerResponseFilt
     @Override
     public void filter(ContainerRequestContext context) throws IOException
     {
+        String payload = IOUtils.toString(context.getEntityStream(), StandardCharsets.UTF_8);
+        //JsonObject input = JsonParser.parseString(payload).getAsJsonObject();
+        logger.info("***********OAuthAgent_Incoming**************");
+        logger.info(payload);
+        logger.info("************************************************");
+
+        context.setEntityStream(new ByteArrayInputStream(payload.getBytes(StandardCharsets.UTF_8)));
     }
 
     @Override
---
+++ src/test/java/io/bugsbunny/SimpleTests.java
@@ -0,0 +1,18 @@
+package io.bugsbunny;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@QuarkusTest
+public class SimpleTests
+{
+    private static Logger logger = LoggerFactory.getLogger(SimpleTests.class);
+
+    @Test
+    public void testSimpleOp() throws Exception
+    {
+
+    }
+}
