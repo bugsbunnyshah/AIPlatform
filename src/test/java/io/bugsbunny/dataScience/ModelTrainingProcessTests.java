f80dc04eabce5d155e2c3af82b74de61e584fb89
babyboy <bugs.bunny.shah@gmail.com>
2020-09-19 18:31:08 -0500

    ai_model_training_process_support

--- src/test/java/io/bugsbunny/dataScience/ModelTrainingProcessTests.java
+++ src/test/java/io/bugsbunny/dataScience/ModelTrainingProcessTests.java
@@ -1,49 +1,23 @@
-/* *****************************************************************************
- * Copyright (c) 2020 Konduit K.K.
- * Copyright (c) 2015-2019 Skymind, Inc.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- ******************************************************************************/
-
 package io.bugsbunny.dataScience;
 
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
-
-import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import io.bugsbunny.dataIngestion.util.CSVDataUtil;
-import io.bugsbunny.dataScience.dl4j.AIPlatformDataSetIterator;
-import io.bugsbunny.dataScience.dl4j.AIPlatformDataSetLoader;
-import io.bugsbunny.dataScience.dl4j.DataSetSourceFactory;
-import io.bugsbunny.dataScience.utils.DownloaderUtility;
 import io.bugsbunny.endpoint.SecurityToken;
 import io.bugsbunny.endpoint.SecurityTokenContainer;
-import io.bugsbunny.persistence.MongoDBJsonStore;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import io.restassured.response.Response;
+
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
-import org.datavec.api.split.FileSplit;
-import org.datavec.api.split.InputStreamInputSplit;
-import org.deeplearning4j.core.loader.DataSetLoader;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.deeplearning4j.datasets.iterator.loader.DataSetLoaderIterator;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
@@ -51,63 +25,34 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.deeplearning4j.util.ModelSerializer;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import org.nd4j.common.loader.*;
-import org.deeplearning4j.datasets.iterator.loader.DataSetLoaderIterator;
-import org.nd4j.linalg.dataset.DataSet;
-
 import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.learning.config.Nesterovs;
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
+import org.datavec.api.split.FileSplit;
+import org.deeplearning4j.util.ModelSerializer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
+import java.util.Base64;
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * "Saturn" Data Classification Example
- *
- * Based on the data from Jason Baldridge:
- * 	https://github.com/jasonbaldridge/try-tf/tree/master/simdata
- *
- * @author Josh Patterson
- * @author Alex Black (added plots)
- *
- */
 @QuarkusTest
-public class ModelTrainingProcessTests {
+public class ModelTrainingProcessTests
+{
     private static Logger logger = LoggerFactory.getLogger(ModelTrainingProcessTests.class);
 
     public static String dataLocalPath;
     public static boolean visualize = true;
-
-    @Inject
-    private CSVDataUtil csvDataUtil;
-
-    @Inject
-    private MongoDBJsonStore mongoDBJsonStore;
-
-    @Inject
-    private AIPlatformDataSetIterator aiPlatformDataSetIterator;
 
     @Inject
     private SecurityTokenContainer securityTokenContainer;
@@ -122,35 +67,61 @@ public class ModelTrainingProcessTests {
         this.securityTokenContainer.getTokenContainer().set(securityToken);
     }
 
-    //@Test
-    public void testModel() throws Exception {
+    @Test
+    public void testModel() throws Exception
+    {
         int batchSize = 50;
         int seed = 123;
         double learningRate = 0.005;
-        //Number of epochs (full passes of the data)
         int nEpochs = 30;
 
         int numInputs = 2;
         int numOutputs = 2;
         int numHiddenNodes = 20;
 
-        dataLocalPath = DownloaderUtility.CLASSIFICATIONDATA.Download();
-        //Load the training data:
-        RecordReader rr = new CSVRecordReader();
-        rr.initialize(new FileSplit(new File(dataLocalPath, "saturn_data_train.csv")));
-        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr, batchSize, 0, 2);
+        String tmp = "tmp";
 
         //Load the test/evaluation data:
-        Response response = given().get("/dataset/readForEval").andReturn();
-        JsonObject dataSet = JsonParser.parseString(response.body().asString()).getAsJsonObject();
-        String csvData = dataSet.get("data").getAsString();
+        String data = IOUtils.resourceToString("dataScience/saturn_data_eval.csv", StandardCharsets.UTF_8,
+                Thread.currentThread().getContextClassLoader());
+        JsonObject input = new JsonObject();
+        input.addProperty("format", "csv");
+        input.addProperty("data", data);
+        Response response = given().body(input.toString()).when().post("/dataset/storeEvalDataSet/").andReturn();
+        JsonObject returnValue = JsonParser.parseString(response.body().asString()).getAsJsonObject();
+        long dataSetId = returnValue.get("dataSetId").getAsLong();
+        response = given().get("/dataset/readDataSet/?dataSetId="+dataSetId).andReturn();
+        returnValue = JsonParser.parseString(response.body().asString()).getAsJsonObject();
+        String storedData = returnValue.get("data").getAsString();
+        String testFileName = UUID.randomUUID().toString();
+        File testFile = new File(tmp+"/"+testFileName);
+        FileUtils.writeStringToFile(testFile, storedData);
         RecordReader rrTest = new CSVRecordReader();
-        InputStreamInputSplit inputStreamInputSplit = new InputStreamInputSplit(new ByteArrayInputStream(
-                csvData.getBytes(UTF_8)));
-        rrTest.initialize(inputStreamInputSplit);
+        //InputStreamInputSplit testSplit = new InputStreamInputSplit(new ByteArrayInputStream(
+        //        storedData.getBytes(UTF_8)));
+        rrTest.initialize(new FileSplit(testFile));
         DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest, batchSize, 0, 2);
 
-        //log.info("Build model....");
+        data = IOUtils.resourceToString("dataScience/saturn_data_train.csv", StandardCharsets.UTF_8,
+                Thread.currentThread().getContextClassLoader());
+        input = new JsonObject();
+        input.addProperty("format", "csv");
+        input.addProperty("data", data);
+        response = given().body(input.toString()).when().post("/dataset/storeTrainingDataSet/").andReturn();
+        returnValue = JsonParser.parseString(response.body().asString()).getAsJsonObject();
+        dataSetId = returnValue.get("dataSetId").getAsLong();
+        response = given().get("/dataset/readDataSet/?dataSetId="+dataSetId).andReturn();
+        returnValue = JsonParser.parseString(response.body().asString()).getAsJsonObject();
+        storedData = returnValue.get("data").getAsString();
+        String trainFileName = UUID.randomUUID().toString();
+        File trainFile = new File(tmp+"/"+trainFileName);
+        FileUtils.writeStringToFile(trainFile, storedData);
+        RecordReader rrTrain = new CSVRecordReader();
+        //InputStreamInputSplit trainSplit = new InputStreamInputSplit(new ByteArrayInputStream(
+        //        storedData.getBytes(UTF_8)));
+        rrTrain.initialize(new FileSplit(trainFile));
+        DataSetIterator trainIter = new RecordReaderDataSetIterator(rrTrain, batchSize, 0, 2);
+
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .seed(seed)
                 .weightInit(WeightInit.XAVIER)
@@ -171,8 +142,9 @@ public class ModelTrainingProcessTests {
 
         model.fit(trainIter, nEpochs);
 
+        System.out.println("Evaluate model....");
         Evaluation eval = model.evaluate(testIter);
-        //logger.info(eval.toJson());
+        System.out.println(eval.stats());
 
         //Deploy the Model
         ByteArrayOutputStream modelBytes = new ByteArrayOutputStream();
@@ -192,10 +164,9 @@ public class ModelTrainingProcessTests {
         response = given().body(packageResponse.body().asString()).when().post("/liveModel/eval").andReturn();
         response.body().prettyPrint();
         assertEquals(200, response.getStatusCode());
-
     }
 
-    //@Test
+    /*@Test
     public void testDataSetIteratorGeneric() throws Exception
     {
         //logger.info("*********DATA_SET_DATA********************");
@@ -240,25 +211,5 @@ public class ModelTrainingProcessTests {
 
         Evaluation eval = model.evaluate(this.aiPlatformDataSetIterator);
         logger.info(eval.toJson());
-    }
-
-    public static void generateVisuals(MultiLayerNetwork model, DataSetIterator trainIter, DataSetIterator testIter) throws Exception {
-        if (visualize) {
-            double xMin = -15;
-            double xMax = 15;
-            double yMin = -15;
-            double yMax = 15;
-
-            //Let's evaluate the predictions at every point in the x/y input space, and plot this in the background
-            int nPointsPerAxis = 100;
-
-            //Generate x,y points that span the whole range of features
-            /*INDArray allXYPoints = PlotUtil.generatePointsOnGraph(xMin, xMax, yMin, yMax, nPointsPerAxis);
-            //Get train data and plot with predictions
-            PlotUtil.plotTrainingData(model, trainIter, allXYPoints, nPointsPerAxis);
-            TimeUnit.SECONDS.sleep(3);
-            //Get test data, run the test data through the network to generate predictions, and plot those predictions:
-            PlotUtil.plotTestData(model, testIter, allXYPoints, nPointsPerAxis);*/
-        }
-    }
+    }*/
 }
