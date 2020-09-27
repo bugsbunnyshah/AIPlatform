08e5939270eb86dae317c303c32d9db9f131d081
babyboy <bugs.bunny.shah@gmail.com>
2020-09-27 16:42:46 -0500

    oauth_flow

--- src/test/java/io/bugsbunny/endpoint/OAuthAuthenticateTests.java
+++ src/test/java/io/bugsbunny/endpoint/OAuthAuthenticateTests.java
@@ -4,9 +4,12 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.response.Response;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
 
 import static io.restassured.RestAssured.given;
 
@@ -18,14 +21,10 @@ public class OAuthAuthenticateTests
     @Test
     public void testOAuthGetStarted() throws Exception
     {
-        String clientId = "PAlDekAoo0XWjAicU9SQDKgy7B0y2p2t";
-        String clientSecret = "U2jMgxL8zJgYOMmHDYTe6-P9yO6Wq51VmixuZSRCaL-11EPE4WrQOWtGLVnQetdd";
-        JsonObject payload = new JsonObject();
-        payload.addProperty("client_id", clientId);
-        payload.addProperty("client_secret", clientSecret);
-        payload.addProperty("audience", "https://appgallabs.us.auth0.com/api/v2/");
-        payload.addProperty("grant_type", "client_credentials");
-        Response response = given().body(payload.toString()).when().post("/oauth/token/").andReturn();
+        String credentials = IOUtils.resourceToString("oauth/credentials.json",
+                StandardCharsets.UTF_8,
+                Thread.currentThread().getContextClassLoader());
+        Response response = given().body(credentials.toString()).when().post("/oauth/token/").andReturn();
         logger.info("************************");
         logger.info(response.statusLine());
         response.body().prettyPrint();
