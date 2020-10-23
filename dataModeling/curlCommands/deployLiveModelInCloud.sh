3c31504d1b3631f4d5867dce50e6546b719e81ca
babyboy <bugs.bunny.shah@gmail.com>
2020-10-23 15:15:54 -0500

    evolving_the_training_to_live_model_process

---
+++ dataModeling/curlCommands/deployLiveModelInCloud.sh
@@ -0,0 +1,5 @@
+curl --location --request POST 'http://localhost:8080/liveModel/deployJavaModel/' \
+--header 'Principal: PAlDekAoo0XWjAicU9SQDKgy7B0y2p2t' \
+--header 'Bearer: blah' \
+--header 'Content-Type: application/json' \
+--data-raw ' {"modelId":-925747089653114721}'
---
+++ dataModeling/curlCommands/trainModelInCloud.sh
@@ -0,0 +1,5 @@
+curl --location --request POST 'http://localhost:8080/trainModel/trainJava/' \
+--header 'Principal: PAlDekAoo0XWjAicU9SQDKgy7B0y2p2t' \
+--header 'Bearer: blah' \
+--header 'Content-Type: application/json' \
+--data-raw ' {"modelId":-925747089653114721,"dataSetIds":["-7094450076213466538"]}'
