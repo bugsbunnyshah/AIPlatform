2633a95a0342cdc0043b2d8a047a0dc1ac97a359
babyboy <bugs.bunny.shah@gmail.com>
2020-11-08 15:57:01 -0600

    aviation_product_getting_started

--- src/test/java/io/bugsbunny/SimpleTests.java
+++ src/test/java/io/bugsbunny/SimpleTests.java
@@ -1,12 +1,13 @@
 package io.bugsbunny;
 
+import io.bugsbunny.test.components.BaseTest;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@QuarkusTest
-public class SimpleTests
+@QuarkusTest
+public class SimpleTests extends BaseTest
 {
     private static Logger logger = LoggerFactory.getLogger(SimpleTests.class);
 
