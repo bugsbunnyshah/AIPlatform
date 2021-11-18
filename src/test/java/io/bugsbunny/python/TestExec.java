163a885ac1be9ec3a0470ad4f6669d4888e64023
babyboy <bugs.bunny.shah@gmail.com>
2021-11-18 18:18:49 -0500

    datascience:jep_integration_python_training_support

--- src/test/java/io/bugsbunny/python/TestExec.java
+++ src/test/java/io/bugsbunny/python/TestExec.java
@@ -4,6 +4,9 @@ import jep.Interpreter;
 import jep.JepException;
 import jep.MainInterpreter;
 import jep.SharedInterpreter;
+import org.python.util.PythonInterpreter;
+
+import java.io.InputStream;
 
 /**
  * Tests ability to execute multiple lines of python using exec()
@@ -31,6 +34,49 @@ public class TestExec
             }
             System.out.println(result);
         }
+
+        try (Interpreter interp = new SharedInterpreter())
+        {
+            String pythonScript = "import pandas as pd\n" +
+                    "output = pd.DataFrame()\n";
+            interp.exec(pythonScript);
+            String output = interp.getValue("output", String.class);
+            System.out.println(output);
+        }
+
+        try (PythonInterpreter pyInterp = new PythonInterpreter()) {
+            InputStream scriptStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(
+                    "cloudml/createModel.py");
+            pyInterp.execfile(scriptStream);
+        }
+
+        /*try (Interpreter interp = new SharedInterpreter())
+        {
+            String pythonScript = "from io.bugsbunny.dataScience.function.model import CreateModel\n" +
+                    "from io.bugsbunny.dataScience.function.model import InputLayer\n" +
+                    "from io.bugsbunny.dataScience.function.model import OutputLayer\n" +
+                    "from org.nd4j.linalg.activations import Activation\n" +
+                    "from org.nd4j.linalg.lossfunctions import LossFunctions\n" +
+                    "from org.deeplearning4j.nn.weights import WeightInit\n" +
+                    "\n" +
+                    "inputLayer = InputLayer(Activation.RELU.name());\n" +
+                    "outputLayer = OutputLayer(Activation.SOFTMAX.name(),\n" +
+                    "                                          LossFunctions.LossFunction.SQUARED_LOSS.name());\n" +
+                    "inputLayers = [inputLayer]\n" +
+                    "\n" +
+                    "outputLayers = [outputLayer]\n" +
+                    "\n" +
+                    "cloudMlFunction = CreateModel(123,\n" +
+                    "                             0.008,\n" +
+                    "                             1,WeightInit.XAVIER.name(),\n" +
+                    "                             inputLayers,\n" +
+                    "                             outputLayers\n" +
+                    ")\n" +
+                    "print(cloudMlFunction.execute())";
+            interp.exec(pythonScript);
+            //String output = interp.getValue("output", String.class);
+            //System.out.println(output);
+        }*/
     }
 
 }
--- src/test/resources/cloudml/createModel.py
+++ src/test/resources/cloudml/createModel.py
@@ -1,9 +1,17 @@
+#import jep
+#import pandas as pd
+from jep import jproxy
 from io.bugsbunny.dataScience.function.model import CreateModel
 from io.bugsbunny.dataScience.function.model import InputLayer
 from io.bugsbunny.dataScience.function.model import OutputLayer
 from org.nd4j.linalg.activations import Activation
 from org.nd4j.linalg.lossfunctions import LossFunctions
 from org.deeplearning4j.nn.weights import WeightInit
+
+#df = pd.DataFrame()
+#print(df)
+pythonList = ['one', 'two']
+javaDeque = jep.jproxy(pythonList, ['java.util.Deque'])
 
 inputLayer = InputLayer(Activation.RELU.name());
 outputLayer = OutputLayer(Activation.SOFTMAX.name(),
